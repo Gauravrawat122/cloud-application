19b818dd19cca7b92e913eeb3d8ff75d7252d9cf
Gauravrawat122 <117441022+Gauravrawat122@users.noreply.github.com>
2023-04-24 20:59:04 +0530

    Add files via upload

---
+++ gymweb/index.js
@@ -0,0 +1,143 @@
+// for cardio
+document.getElementById("btn1").onclick=function() {
+    fun()
+};
+
+const arr = [
+    { title: "Upper body cardio", from: "ghhgg", iframe: "hjghjhgjg" },
+    { title: "Lower body cardio", from: "=gbhgbhg", iframe: "s=hgjgjg" },
+    { title: "Full body cardio", from: "kjbfkbk", iframe: "fsdjijv" }
+   
+];
+
+function fun() {
+    const ul = document.getElementsByTagName('ul')[0];
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    ul.innerHTML = `
+        <li>
+            Song: ${item.title}, singer: ${item.from}
+            <iframe src="${item.iframe}"></iframe>
+            <a href="${item.iframe}" class="link">Link</a>
+        </li>`;
+}
+
+// for weights
+
+document.getElementById("btn2").onclick=function() {
+    fun()
+};
+
+const arr1 = [
+    { title: "Upper body cardio", from: "ghhgg", iframe: "hjghjhgjg" },
+    { title: "Lower body cardio", from: "=gbhgbhg", iframe: "s=hgjgjg" },
+    { title: "Full body cardio", from: "kjbfkbk", iframe: "fsdjijv" }
+   
+];
+
+function fun() {
+    const ul = document.getElementsByTagName('ul')[0];
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    ul.innerHTML = `
+        <li>
+            Song: ${item.title}, singer: ${item.from}
+            <iframe src="${item.iframe}"></iframe>
+            <a href="${item.iframe}" class="link">Link</a>
+        </li>`;
+}
+
+// for diet
+
+document.getElementById("btn3").onclick=function() {
+    fun()
+};
+
+const arr2 = [
+    { title: "Upper body cardio", from: "ghhgg", iframe: "hjghjhgjg" },
+    { title: "Lower body cardio", from: "=gbhgbhg", iframe: "s=hgjgjg" },
+    { title: "Full body cardio", from: "kjbfkbk", iframe: "fsdjijv" }
+   
+];
+
+function fun() {
+    const ul = document.getElementsByTagName('ul')[0];
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    ul.innerHTML = `
+        <li>
+            Song: ${item.title}, singer: ${item.from}
+            <iframe src="${item.iframe}"></iframe>
+            <a href="${item.iframe}" class="link">Link</a>
+        </li>`;
+}
+
+// for personal coach
+
+document.getElementById("btn4").onclick=function() {
+    fun()
+};
+
+const arr3 = [
+    { title: "Upper body cardio", from: "ghhgg", iframe: "hjghjhgjg" },
+    { title: "Lower body cardio", from: "=gbhgbhg", iframe: "s=hgjgjg" },
+    { title: "Full body cardio", from: "kjbfkbk", iframe: "fsdjijv" }
+   
+];
+
+function fun() {
+    const ul = document.getElementsByTagName('ul')[0];
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    ul.innerHTML = `
+        <li>
+            Song: ${item.title}, singer: ${item.from}
+            <iframe src="${item.iframe}"></iframe>
+            <a href="${item.iframe}" class="link">Link</a>
+        </li>`;
+}
+
+//for certifications
+
+document.getElementById("btn5").onclick=function() {
+    fun()
+};
+
+const arr4 = [
+    { title: "Upper body cardio", from: "ghhgg", iframe: "hjghjhgjg" },
+    { title: "Lower body cardio", from: "=gbhgbhg", iframe: "s=hgjgjg" },
+    { title: "Full body cardio", from: "kjbfkbk", iframe: "fsdjijv" }
+   
+];
+
+function fun() {
+    const ul = document.getElementsByTagName('ul')[0];
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    ul.innerHTML = `
+        <li>
+            Song: ${item.title}, singer: ${item.from}
+            <iframe src="${item.iframe}"></iframe>
+            <a href="${item.iframe}" class="link">Link</a>
+        </li>`;
+}
+
+//for zumba
+
+document.getElementById("btn6").onclick=function() {
+    fun()
+};
+
+const arr5 = [
+    { title: "Upper body cardio", from: "ghhgg", iframe: "hjghjhgjg" },
+    { title: "Lower body cardio", from: "=gbhgbhg", iframe: "s=hgjgjg" },
+    { title: "Full body cardio", from: "kjbfkbk", iframe: "fsdjijv" }
+   
+];
+
+function fun() {
+    const ul = document.getElementsByTagName('ul')[0];
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    ul.innerHTML = `
+        <li>
+            Song: ${item.title}, singer: ${item.from}
+            <iframe src="${item.iframe}"></iframe>
+            <a href="${item.iframe}" class="link">Link</a>
+        </li>`;
+}
+
